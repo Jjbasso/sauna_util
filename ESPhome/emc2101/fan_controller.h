5e2f3f45dc6ae4f508fe95b5438b853f70f813e8
jjbasso <git.dnd.33mail.com>
2022-11-20 13:51:54 -0600

    Added Stub

--- ESPhome/emc2101/fan_controller.h
+++ ESPhome/emc2101/fan_controller.h
@@ -1,1 +1,11 @@
-// Testing Push In Visual Studio
+#include "esphome.h"
+
+class MyCustomSensor : public Component, public Sensor {
+ public:
+  void setup() override {
+    // This will be called by App.setup()
+  }
+  void loop() override {
+    // This will be called by App.loop()
+  }
+};
