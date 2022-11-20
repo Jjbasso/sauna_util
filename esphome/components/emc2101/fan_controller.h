d1f248571194e255ab3c4675ac0458736818b078
jjbasso <git.dnd.33mail.com>
2022-11-20 14:34:16 -0600

    Update

---
+++ esphome/components/emc2101/fan_controller.h
@@ -0,0 +1,13 @@
+#include "esphome.h"
+
+class MyCustomSensor : public Component, public Sensor {
+ public:
+  void setup() override {
+    // This will be called by App.setup()
+    //
+  }
+  void loop() override {
+    // This will be called by App.loop()
+    //  
+  }
+};
