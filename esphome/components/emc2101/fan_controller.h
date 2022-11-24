163f8dc5b9b4394dfa617d854e2af5f61ae852a9
Jeff Basso <git@dnd.33mail.com>
2022-11-24 00:31:29 -0600

    Update fan_controller.h

--- esphome/components/emc2101/fan_controller.h
+++ esphome/components/emc2101/fan_controller.h
@@ -56,9 +56,17 @@ class emc2101_fan_switch : public Component, public Switch {
   public:
    
     void write_state(float state) override {
+     // if we are not manually setting fan speed then put fan in auto enable mode
+     // based on onboard teamp sensor and lookup table
+     if (value == 0.0) {
+      emc.LUTEnabled(true);
+    }
+  else {
+      emc.LUTEnabled(false);
+  }
       
      // state is the amount this output should be on, from 0.0 to 1.0
-     // we need to convert it to an integer first
+     // We multiple by 19 and not 100 due to calc error or resolution problem
      int value = state * 19;
      emc.setDutyCycle(value);
     }
