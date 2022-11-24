18444ebc635aae582401f73e34b925c7da25f860
Jeff Basso <git@dnd.33mail.com>
2022-11-24 01:44:54 -0600

    Update fan_controller.h

--- esphome/components/emc2101/fan_controller.h
+++ esphome/components/emc2101/fan_controller.h
@@ -50,18 +50,17 @@ class emc2101_fan_speed : public Component, public FloatOutput {
         emc.setDutyCycle(value);
      }
      else {
-        emc.setDutyCycle(0);
         
        // This will be called by App.setup()
        // LUT for auto fan mode
-       emc.setLUT(0, (90-32)*.5556, 1);
-       emc.setLUT(1, (95-32)*.5556, 1);
-       emc.setLUT(2, (100-32)*.5556, 1);
-       emc.setLUT(3, (105-32)*.5556, 1);
-       emc.setLUT(4, (110-32)*.5556, 1);
-       emc.setLUT(5, (115-32)*.5556, 1);
-       emc.setLUT(6, (120-32)*.5556, 1);
-       emc.setLUT(7, (125-32)*.5556, 1);
+       emc.setLUT(0, (32-32)*.5556, 0);
+       emc.setLUT(1, (95-32)*.5556, 13);
+       emc.setLUT(2, (100-32)*.5556, 14);
+       emc.setLUT(3, (105-32)*.5556, 15);
+       emc.setLUT(4, (110-32)*.5556, 16);
+       emc.setLUT(5, (115-32)*.5556, 17);
+       emc.setLUT(6, (120-32)*.5556, 18);
+       emc.setLUT(7, (125-32)*.5556, 19);
        emc.LUTEnabled(true);
        // use for testing loookup table  
        // emc.enableForcedTemperature(true);
