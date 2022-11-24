01d439c06ed2ba34506625b2aed3a76c3550be8a
Jeff Basso <git@dnd.33mail.com>
2022-11-24 01:30:02 -0600

    Update fan_controller.h

--- esphome/components/emc2101/fan_controller.h
+++ esphome/components/emc2101/fan_controller.h
@@ -46,6 +46,7 @@ class emc2101_fan_speed : public Component, public FloatOutput {
      // based on onboard teamp sensor and lookup table
      if (value > 0) {
         emc.LUTEnabled(false);
+        emc2101.enableForcedTemperature(false);
         emc.setDutyCycle(value);
      }
      else {
@@ -62,7 +63,8 @@ class emc2101_fan_speed : public Component, public FloatOutput {
        emc.setLUT(6, (120-32)*.5556, 18);
        emc.setLUT(7, (125-32)*.5556, 19);
        emc.LUTEnabled(true);
-       // use for testing loookup table   
+       // use for testing loookup table  
+       // emc2101.enableForcedTemperature(true);
        // emc.setForcedTemperature((108-32)*.5556);
      }
     }
