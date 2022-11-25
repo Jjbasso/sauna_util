f3737813a8b7e26b749e36e9473d922180ede566
Jeff Basso <git@dnd.33mail.com>
2022-11-25 10:23:03 -0600

    Update fan_tuning.ino

--- src/fan_tuning.ino
+++ src/fan_tuning.ino
@@ -6,7 +6,7 @@
 Adafruit_EMC2101  emc2101;
 void setup(void) {
   Serial.begin(115200);
-  while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+  while (!Serial) delay(10);  
 
   Serial.println("Adafruit EMC2101 test!");
 
@@ -17,97 +17,53 @@ void setup(void) {
   }
   Serial.println("EMC2101 Found!");
 
-   emc2101.setLUT(0, (90-32)*.5556, 40);
-   emc2101.setLUT(1, (100-32)*.5556, 80);   
-   emc2101.setLUT(2, (110-32)*.5556, 90);
-   emc2101.setLUT(3, (120-32)*.5556, 100);
+   emc2101.setLUT(0, (32-32)*.5556, 12);
+   emc2101.setLUT(1, (95-32)*.5556, 13);
+   emc2101.setLUT(2, (100-32)*.5556, 14);
+   emc2101.setLUT(3, (105-32)*.5556, 15);
+   emc2101.setLUT(4, (110-32)*.5556, 16);
+   emc2101.setLUT(5, (115-32)*.5556, 17);
+   emc2101.setLUT(6, (120-32)*.5556, 18);
+   emc2101.setLUT(7, (125-32)*.5556, 19);
    
-    emc2101.setFanMinRPM(1000);
-    emc2101.configFanSpinup(true);
-    emc2101.invertFanSpeed(false);
-  delay(5000);
-  Serial.print("Fan RPM:: "); Serial.println(emc2101.getFanRPM());
-  delay(20000);
-}
+   
+    emc2101.LUTEnabled(false);
+    emc2101.setFanMinRPM(350);  // Spec sheet Min Speed
+    emc2101.configFanSpinup(true);  // Run fan at 100% duty  until it hits min rpm setting
 
-void loop() {
-  emc2101.LUTEnabled(false);
-  delay(5000);
-
+    emc2101.enableTachInput(true);
+    emc2101.configPWMClock(false,true);
+    emc2101.setPWMDivisor(0);
+    emc2101.setPWMFrequency(7);
+   
+   
   Serial.print("Internal Temperature: ");
   Serial.print((emc2101.getInternalTemperature()*1.8)+32);Serial.println(" degrees F");
 
-  emc2101.setDutyCycle(0);
-  Serial.println("Fan duty cycle: 0");
-  delay(5000);
-  Serial.print("Fan RPM:: "); Serial.println(emc2101.getFanRPM());
-
-  emc2101.setDutyCycle(40);
-  Serial.println("Fan duty cycle: 40");
-  delay(5000);
-  Serial.print("Fan RPM:: "); Serial.println(emc2101.getFanRPM());
-
-  emc2101.setDutyCycle(50);
-  Serial.println("Fan duty cycle: 50");
-  delay(5000);
-  Serial.print("Fan RPM:: "); Serial.println(emc2101.getFanRPM());
-
-  emc2101.setDutyCycle(60);
-  Serial.println("Fan duty cycle: 60");
-  delay(5000);
-  Serial.print("Fan RPM:: "); Serial.println(emc2101.getFanRPM());
-
-  emc2101.setDutyCycle(70);
-  Serial.println("Fan duty cycle: 70");
-  delay(5000);
-  Serial.print("Fan RPM:: "); Serial.println(emc2101.getFanRPM());
-
-  emc2101.setDutyCycle(80);
-  Serial.println("Fan duty cycle: 80");
-  delay(5000);
-  Serial.print("Fan RPM:: "); Serial.println(emc2101.getFanRPM());
-
-  emc2101.setDutyCycle(90);
-  Serial.println("Fan duty cycle: 90");
-  delay(5000);
-  Serial.print("Fan RPM:: "); Serial.println(emc2101.getFanRPM());
-
-  emc2101.setDutyCycle(100);
-  Serial.println("Fan duty cycle: 100");
-  delay(5000);
-  Serial.print("Fan RPM:: "); Serial.println(emc2101.getFanRPM());
-
+  int i;
+  for (i = 0; i < 100; ++i) {
+    emc2101.setDutyCycle(i);
+    delay(3000);
+    Serial.print("Duty ");Serial.print(i);Serial.print("||Fan RPM:: "); Serial.println(emc2101.getFanRPM());
+  }
   
-  Serial.println("Change to Internal Lookup table to run fan");
-  emc2101.LUTEnabled(true);
-  emc2101.setLUTHysteresis(5); // 5 degree C fudge factor
-  emc2101.enableForcedTemperature(true);
-  delay(5000);
+   Serial.println("Change to Internal Lookup table to run fan");
+   emc2101.LUTEnabled(true);
+   emc2101.enableForcedTemperature(true);
+
+   int i;
+  for (i = 75; i < 128; ++i) {
+    emc2101.setDutyCycle(i);
+    delay(2000);
+    emc2101.setForcedTemperature((i-32)*.5556);
+    Serial.print("Temp ");Serial.print(i);Serial.print("||Fan RPM "); Serial.println(emc2101.getFanRPM());
+  }
   
-  Serial.print("Forcing to 75 degrees F: ");
-  emc2101.setForcedTemperature((75-32)*.5556);
-  Serial.print("Forced Temperature: ");Serial.print((emc2101.getForcedTemperature()*1.8)+32);Serial.println(" degrees F");
-  delay(5000);
  
-  Serial.print("Forcing to 90 degrees F: ");
-  emc2101.setForcedTemperature((90-32)*.5556);
-  Serial.print("Forced Temperature: ");Serial.print((emc2101.getForcedTemperature()*1.8)+32);Serial.println(" degrees F");
-  delay(5000);
+}
 
-  Serial.print("Forcing to 100 degrees F: ");
-  emc2101.setForcedTemperature((100-32)*.5556);
-  Serial.print("Forced Temperature: ");Serial.print((emc2101.getForcedTemperature()*1.8)+32);Serial.println(" degrees F");
-  delay(5000);
+void loop() {
   
-  Serial.print("Forcing to 110 degrees F: ");
-  emc2101.setForcedTemperature((110-32)*.5556);
-  Serial.print("Forced Temperature: ");Serial.print((emc2101.getForcedTemperature()*1.8)+32);Serial.println(" degrees F");
-  delay(5000);
-
-  Serial.print("Forcing to 120 degrees F: ");
-  emc2101.setForcedTemperature((120-32)*.5556);
-  Serial.print("Forced Temperature: ");Serial.print((emc2101.getForcedTemperature()*1.8)+32);Serial.println(" degrees F");
-  delay(5000);
 
   
 }
